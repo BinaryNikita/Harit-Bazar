c811de18bec85e6cf58fa7eeb7fdadd6b1003cb3
Nikita Vishnoi <nikita.works32@gmail.com>
2024-12-29 10:55:34 +0530

    assigned server port

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 import express  from 'express';
+import { connection } from './config/dbConfig.js';
+
+const app = express();
+
+
+
+app.listen(4000, () => {
+    console.log("server started....");
+})
