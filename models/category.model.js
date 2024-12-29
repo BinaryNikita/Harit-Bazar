05d7676dcaee1c275d60ec74e790a1181cb023c7
Nikita Vishnoi <nikita.works32@gmail.com>
2024-12-29 11:15:31 +0530

    removed category id

--- models/category.model.js
+++ models/category.model.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const CategorySchema = new mongoose.Schema({
-    category_id: { type: String, required: true, unique: true },
     categoryName: { type: String, required: true },
     subCategory: { type: String }
 });
